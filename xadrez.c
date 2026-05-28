12d215f8d6c29672ca739201ac95bbb8945476ff
Gabriel Medeiros Patrocinio <62565345+gabs73@users.noreply.github.com>
2026-05-28 18:01:38 +0000

    Concluí nivel novato

--- xadrez.c
+++ xadrez.c
@@ -11,52 +11,96 @@ int main() {
     int dir_bishop, dir_rook, dir_queen;    //direção que cada peça moverá
 
 
-    //Movimentação Bispo
-    printf("Informe quantas casas o bispo andará\n");
-        scanf("%d",&mov_bishop);
+//Movimentação Bispo
+printf("Informe quantas casas o bispo andará\n");
+    scanf("%d",&mov_bishop);
 
-    printf("Informe a direção que o bispo andará:\n");
-        printf("1:Diagonal direita superior\n");
-        printf("2:Diagonal direita inferior\n");
-        printf("3:Diagonal esquerda superior\n");
-        printf("4:Diagonal esquerda inferior\n");
-            scanf("%d",&dir_bishop);
+printf("Informe a direção que o bispo andará:\n");
+    printf("1:Diagonal direita superior\n");
+    printf("2:Diagonal direita inferior\n");
+    printf("3:Diagonal esquerda superior\n");
+    printf("4:Diagonal esquerda inferior\n");
+        scanf("%d",&dir_bishop);
 
-    //Movimentação Torre
-    printf("Informe quantas casas a torre andará\n");
-        scanf("%d",&mov_rook);
+//Movimentação Torre
+printf("Informe quantas casas a torre andará\n");
+    scanf("%d",&mov_rook);
 
-    printf("Informe a direção que a torre andará:\n");
-        printf("1:Direita\n");
-        printf("2:Esquerda\n");
-        printf("3:Cima\n");
-        printf("4:Baixo\n");
-            scanf("%d",&dir_rook);
+printf("Informe a direção que a torre andará:\n");
+    printf("1:Direita\n");
+    printf("2:Esquerda\n");
+    printf("3:Cima\n");
+    printf("4:Baixo\n");
+        scanf("%d",&dir_rook);
             
-    //Movimentação Rainha
-    printf("Informe quantas casas a rainha andará\n");
-        scanf("%d",&mov_queen);
+//Movimentação Rainha
+printf("Informe quantas casas a rainha andará\n");
+    scanf("%d",&mov_queen);
 
-    printf("Informe a direção que a torre andará:\n");
-        printf("1:Diagonal direita superior\n");
-        printf("2:Diagonal direita inferior\n");
-        printf("3:Diagonal esquerda superior\n");
-        printf("4:Diagonal esquerda inferior\n");
-        printf("5:Direita\n");
-        printf("6:Esquerda\n");
-        printf("7:Cima\n");
-        printf("8:Baixo\n");
-            scanf("%d",&dir_queen);
+printf("Informe a direção que a torre andará:\n");
+    printf("1:Diagonal direita superior\n");
+    printf("2:Diagonal direita inferior\n");
+    printf("3:Diagonal esquerda superior\n");
+    printf("4:Diagonal esquerda inferior\n");
+    printf("5:Direita\n");
+    printf("6:Esquerda\n");
+    printf("7:Cima\n");
+    printf("8:Baixo\n");
+        scanf("%d",&dir_queen);
 
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-    
+    for(int i=1;i<=mov_bishop;i++){
+        if(dir_bishop==1){
+            printf("Bispo andou uma casa para frente e uma casa para direita.\n");
+        }else if(dir_bishop==2){
+            printf("Bispo andou uma casa para trás e uma casa para direita.\n");
+        }else if (dir_bishop==3){
+            printf("Bispo andou uma casa para frente e uma casa para esquerda.\n");
+        }else if (dir_bishop==4){
+            printf("Bispo andou uma casa para trás e uma casa para esquerda.\n");
+        }else{
+            printf("Erro");
+        }
+    }
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
+    for(int i=1;i<=mov_rook;i++){
+        if(dir_rook==1){
+            printf("Torre andou uma casa para direita.\n");
+        }else if(dir_rook==2){
+            printf("Torre andou uma casa para esquerda.\n");
+        }else if (dir_rook==3){
+            printf("Torre andou uma casa para frente.\n");
+        }else if (dir_rook==4){
+            printf("Torre andou uma casa para trás.\n");
+        }else{
+            printf("Erro");
+        }
+    }
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
+    for(int i=1;i<=mov_bishop;i++){
+        if(dir_queen==1){
+            printf("Rainha andou uma casa para frente e uma casa para direita.\n");
+        }else if(dir_queen==2){
+            printf("Rainha andou uma casa para trás e uma casa para direita.\n");
+        }else if (dir_queen==3){
+            printf("Rainha andou uma casa para frente e uma casa para esquerda.\n");
+        }else if (dir_queen==4){
+            printf("Rainha andou uma casa para trás e uma casa para esquerda.\n");
+        }else if(dir_queen==5){
+            printf("Rainha andou uma casa para direita.\n");
+        }else if(dir_queen==6){
+            printf("Rainha andou uma casa para esquerda.\n");
+        }else if (dir_queen==7){
+            printf("Rainha andou uma casa para frente.\n");
+        }else if (dir_queen==8){
+            printf("Rainha andou uma casa para trás.\n");
+        }else{
+            printf("Erro");
+        }
+    }
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
